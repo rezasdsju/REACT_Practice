38bde38c7de8c5b4d250ef929c94ed99f18d4c9a
rezasdsju <rezaul2002104@gmail.com>
2025-11-20 16:56:11 +0600

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,74 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+/*
+function MyButton() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <button>
+      I'm a button
+    </button>
   );
 }
 
-export default App;
+export default function MyApp() {
+  return (
+    <div>
+      <h1>Welcome to my app</h1>
+      <MyButton />
+    </div>
+  );
+} */
+
+/*
+const user = {
+  name: "Jahangirnagar University",
+  imageUrl: "https://juniv.edu/storage/image/gallery/OZPXeyoZlC6hmxyFZbzrvLYU4jSC0LwOxJYLtt8u.jpg",
+  imageSize: 90,
+};
+export default function Profile() {
+  return (
+    <>
+    <h1>{user.name}</h1>
+    <img
+      className= "avatar"
+      src = {user.imageUrl}
+      alt = {"Photo of " + user.name}
+      style= {{width: user.imageSize, height: user.imageSize}}
+    />
+    </>
+  );
+}
+*/
+/*
+const products= [
+  {title: 'Cabbage', isFruit: false, id: 1},
+  {title: 'Garlic', isFruit: false, id: 2},
+  {title: 'Apple', isFruit: true, id: 3},
+]
+export default function ShoppingList() {
+  const listItems= products.map(product=>
+    <li
+      key= {product.id}
+      style = {{color: product.isFruit ? 'magenta' : 'blue'}}
+    >
+      {product.title}
+    </li>
+  );
+  return (
+    <ul>{listItems} </ul>
+  );
+}
+*/
+
+function MyButton() {
+  function handleClick() {
+    alert("You clicked me!");
+  }
+  return (
+    <button onClick={handleClick}>Click Me</button>
+  );
+}
+export default function MyApp() {
+  return (
+    <>
+    <MyButton />
+    </>
+  );
+}
